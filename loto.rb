7cc399998ad113cdd084747d430fd9d92f7b8528
Goran Milovanovic <goran@effectiva.hr>
2015-12-29 20:34:25 +0100

    second version

--- loto.rb
+++ loto.rb
@@ -3,7 +3,7 @@ puts "Please enter your numbers:"
 user_numbers = gets.split.map(&:to_i)
 comp_numbers = 7.times.map{rand(39)+1}
 hits = user_numbers & comp_numbers
-print comp_numbers
+print "Computer numbers: #{comp_numbers}"
 puts 
 puts "You have #{hits.length} hits."
 puts hits
