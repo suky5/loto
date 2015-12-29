ff6f250cd2ed4061e4f4af445386773500d2f48e
Goran Milovanovic <goramm@gmail.com>
2015-12-29 20:25:55 +0100

    first version

---
+++ loto.rb
@@ -0,0 +1,13 @@
+puts "Welcome"
+puts "Please enter your numbers:"
+user_numbers = gets.split.map(&:to_i)
+comp_numbers = 7.times.map{rand(39)+1}
+hits = user_numbers & comp_numbers
+print comp_numbers
+puts 
+puts "You have #{hits.length} hits."
+puts hits
+
+
+
+
